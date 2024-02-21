8ac45bd6575f0bd45614123ec329d3c77051e9ec
王凤祥 <1205679862@qq.com>
2024-02-21 15:40:21 +0800

    feat: 项目初始

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<!--
+ * @Description: 项目首页
+ * @Author: wangfengxiang
+ * @Date: 2024-02-21 15:19:21
+ * @LastEditTime: 2024-02-21 15:33:21
+ * @LastEditors: wangfengxiang
+-->
+<template>
+  <div>项目入口</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+/*
+ * @Description: 项目入口
+ * @Author: wangfengxiang
+ * @Date: 2024-02-21 15:19:21
+ * @LastEditTime: 2024-02-21 15:33:02
+ * @LastEditors: wangfengxiang
+ */
+import { createApp } from "vue";
+import App from "./App.vue";
+
+createApp(App).mount("#app");
