7b03eae54dae969304adc2f858c53075be6b1344
王凤祥 <1205679862@qq.com>
2024-02-21 18:04:44 +0800

    feat：配置tailwind

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@
  * @Description: 项目入口
  * @Author: wangfengxiang
  * @Date: 2024-02-21 15:19:21
- * @LastEditTime: 2024-02-21 15:33:02
+ * @LastEditTime: 2024-02-21 17:42:51
  * @LastEditors: wangfengxiang
  */
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import App from './App.vue'
 
-createApp(App).mount("#app");
+import './style/tailwind.css'
+
+createApp(App).mount('#app')
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/*
+ * @Description:
+ * @Author: wangfengxiang
+ * @Date: 2024-02-21 17:39:07
+ * @LastEditTime: 2024-02-21 17:41:31
+ * @LastEditors: wangfengxiang
+ */
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
