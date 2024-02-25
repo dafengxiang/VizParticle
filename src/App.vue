25a61fce4272b696f2a0adabe55ba4741e97db9f
王凤祥 <1205679862@qq.com>
2024-02-25 21:58:21 +0800

    feat: logo

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,11 @@
  * @Description: 项目首页
  * @Author: wangfengxiang
  * @Date: 2024-02-21 15:19:21
- * @LastEditTime: 2024-02-21 15:33:21
+ * @LastEditTime: 2024-02-22 09:48:07
  * @LastEditors: wangfengxiang
 -->
 <template>
-    <div>项目入口</div>
+    <div class="w-10 border-spacing-x-10">项目入口</div>
 </template>
 
 <script setup lang="ts"></script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,12 @@
  * @Description:
  * @Author: wangfengxiang
  * @Date: 2024-02-21 17:39:07
- * @LastEditTime: 2024-02-21 17:41:31
+ * @LastEditTime: 2024-02-22 09:44:16
  * @LastEditors: wangfengxiang
  */
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    content: ['./index.html', './src/*.{vue,js,ts,jsx,tsx}', './src/**/*.{vue,js,ts,jsx,tsx}'],
     theme: {
         extend: {},
     },
